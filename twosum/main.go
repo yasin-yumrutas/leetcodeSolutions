ba5937b2827343b057b081a23bd569246cbd9fa5
yasin-yumrutas <yasinyumrutas0@gmail.com>
2025-03-05 03:39:10 +0300

    twosum problem solved ~Yasin Yumrutaş

--- twosum/main.go
+++ twosum/main.go
@@ -1,1 +1,22 @@
-//Two Sum Solution
+package main
+
+import "fmt"
+
+func twoSum(nums []int, target int) []int {
+    numMap := make(map[int]int)
+    for i, num := range nums {
+        complement := target - num
+        if index, found := numMap[complement]; found {
+            return []int{index, i}
+        }
+        numMap[num] = i
+    }
+    return nil
+}
+
+func main() {
+    nums := []int{2, 7, 11, 15}
+    target := 9
+    result := twoSum(nums, target)
+    fmt.Println(result)
+}
